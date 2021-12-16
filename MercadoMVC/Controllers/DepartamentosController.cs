3db50e260ce17800d2429b3e5c071d759d263670
DESKTOP-HVIGO08\Miloch <matheus.miloch@targetwork.com.br>
2021-12-15 22:12:51 -0300

    Adicionando classe Departamentos

---
+++ MercadoMVC/Controllers/DepartamentosController.cs
@@ -0,0 +1,21 @@
+﻿using MercadoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MercadoMVC.Controllers
+{
+    public class DepartamentosController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Departamentos> lista = new List<Departamentos>();
+            lista.Add(new Departamentos { Id = 1, Name = "Eletronicos" });
+            lista.Add(new Departamentos { Id = 2, Name = "Alimentos" });
+
+            return View(lista);
+        }
+    }
+}
--- MercadoMVC/Controllers/HomeController.cs
+++ MercadoMVC/Controllers/HomeController.cs
@@ -1,11 +1,7 @@
 ﻿using MercadoMVC.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MercadoMVC.Controllers
 {
