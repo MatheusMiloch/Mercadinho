7d029ca23f5b624f0afd283fd11eabcb1794b4dc
DESKTOP-HVIGO08\Miloch <matheus.miloch@targetwork.com.br>
2021-12-15 23:03:12 -0300

    Adicionando CRUD

---
+++ MercadoMVC/Data/MercadoMVCContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MercadoMVC.Models;
+
+namespace MercadoMVC.Data
+{
+    public class MercadoMVCContext : DbContext
+    {
+        public MercadoMVCContext (DbContextOptions<MercadoMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MercadoMVC.Models.Departamentos> Departamentos { get; set; }
+    }
+}
